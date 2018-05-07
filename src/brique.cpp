a2515b2cbeafc6f482e1342e9de661705706bdbd
Nico <chrijonv@gmail.com>
2018-05-08 01:18:34 +0200

    Collisions briques
     - Correction du bug qui modifiait la mauvaise trajectoire dans certains cas
     - Gestion avancée des collisions sur les coins, en fonction de l'angle d'arrivée et du coin

--- src/brique.cpp
+++ src/brique.cpp
@@ -25,14 +25,60 @@ void Brique::traiterCollision(Balle* &balle)
 {
     float x = balle->getCentreX();
     float y = balle->getCentreY();
-    float rayon = balle->getRayon();
+    float dirX = balle->getDirectionX();
+    float dirY = balle->getDirectionY();
 
-    if (x+rayon>m_position[0] && x-rayon<m_position[0]+m_largeur)
-        balle->setDirection(balle->getDirectionX(), -balle->getDirectionY());
-    else if (y-rayon<m_position[1] && y+rayon>m_position[1]-m_hauteur)
-        balle->setDirection(-balle->getDirectionX(), balle->getDirectionY());
+    // Si le centre de la balle est en-dessous du côté du dessous ou au-dessus du côté du dessus, mais pas à gauche du côté gauche ou à droite du côté droit
+    if ((y<m_position[1]-m_hauteur || y>m_position[1]) && !(x<m_position[0] || x>m_position[0]+m_largeur))
+        balle->setDirection(dirX, -dirY);
+
+    // Si le centre de la balle est à gauche du côté gauche ou à droite du côté droit, mais pas en-dessous du côté du dessous ou au-dessus du côté du dessus
+    else if ((x<m_position[0] || x>m_position[0]+m_largeur) && !(y<m_position[1]-m_hauteur || y>m_position[1]))
+        balle->setDirection(-dirX, dirY);
+
+    // Sinon il s'agit d'un coin
     else
-        balle->setDirection(-balle->getDirectionX(), -balle->getDirectionY());
+    {
+        if (x<m_position[0] && y>m_position[1]) // Coin haut gauche
+        {
+            if (dirX>0.0f && dirY>=0.0f) // Angle d'arrivée : ]-90;180]
+                balle->setDirection(-dirX, dirY);
+            else if (dirX<=0.0f && dirY<0.0f) // Angle d'arrivée : ]0;90]
+                balle->setDirection(dirX, -dirY);
+            else // Angle d'arrivée : ]90;180[
+                balle->setDirection(-dirX, -dirY);
+        }
+
+        else if (x>m_position[0]+m_largeur && y>m_position[1]) // Coin haut droit
+        {
+            if (dirX>=0.0f && dirY<0.0f) // Angle d'arrivée : [90;180[
+                balle->setDirection(dirX, -dirY);
+            else if (dirX<0.0f && dirY>=0.0f) // Angle d'arrivée : [0;-90[
+                balle->setDirection(-dirX, dirY);
+            else // Angle d'arrivée : ]90;180[
+                balle->setDirection(-dirX, -dirY);
+        }
+
+        else if (x>m_position[0]+m_largeur && y<m_position[1]-m_hauteur) // Coin bas droit
+        {
+            if (dirX<0.0f && dirY<=0.0f) // Angle d'arrivée : [0;90[
+                balle->setDirection(-dirX, dirY);
+            else if (dirX>=0.0f && dirY>0.0f) // Angle d'arrivée : [-90;-180[
+                balle->setDirection(dirX, -dirY);
+            else // Angle d'arrivée : ]90;180[
+                balle->setDirection(-dirX, -dirY);
+        }
+
+        else if (x<m_position[0] && y<m_position[1]-m_hauteur) // Coin bas gauche
+        {
+            if (dirX<=0.0f && dirY>0.0f) // Angle d'arrivée : ]0;-90]
+                balle->setDirection(dirX, -dirY);
+            else if (dirX>0.0f && dirY<=0.0f) // Angle d'arrivée : ]90;180]
+                balle->setDirection(-dirX, dirY);
+            else // Angle d'arrivée : ]90;180[
+                balle->setDirection(-dirX, -dirY);
+        }
+    }
 }
 
 void Brique::Display()//j'affiche la brique avec un rapport 1 de hauteur pour 3 de largeur
